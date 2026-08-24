7257a77022397024263acdad49dde89a41dde27c
tgbao <tgbao@192.168.2.11>
2026-08-24 16:53:05 +0900

    chore: remove test, debug, and temp files

--- tests/test_cobalt.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-test('test cobalt download', async ({ page }) => {
-  await page.goto('https://cobalt.tools/');
-  const html = await page.innerHTML('body');
-  const fs = require('fs');
-  fs.writeFileSync('cobalt.html', html);
-});
--- tests/test_dom.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-test('Dump DOM', async ({ page }) => {
-  await page.goto('http://localhost:3000/shorts');
-  await page.waitForTimeout(3000);
-  const html = await page.evaluate(() => document.querySelector('.short-container')?.innerHTML);
-  console.log("HTML of first short:\n", html);
-});
